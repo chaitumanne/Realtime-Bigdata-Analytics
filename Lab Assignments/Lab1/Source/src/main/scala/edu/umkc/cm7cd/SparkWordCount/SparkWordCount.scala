0ef24fe5a0220fc162aa5b28e403b1c821d2ab07
Chaitanya Sai Manne <cm7cd@mail.umkc.edu>
2016-08-29 10:31:49 -0500

    source code uploaded

--- Lab Assignments/Lab1/Source/src/main/scala/edu/umkc/cm7cd/SparkWordCount/SparkWordCount.scala
+++ Lab Assignments/Lab1/Source/src/main/scala/edu/umkc/cm7cd/SparkWordCount/SparkWordCount.scala
@@ -14,7 +14,9 @@ object SparkWordCount {
 
     val wc=input.flatMap(line=>{line.split(" ")}).map(word=>(word,1))
 
-    val output=wc.reduceByKey(_+_).map(tuple => (tuple._2, tuple._1)).sortByKey(true)
+    //val output=wc.reduceByKey(_+_).map(tuple => (tuple._2, tuple._1)).sortByKey(true)
+
+    val output=wc.reduceByKey(_+_).map(item => item.swap).sortByKey(true,1).map(item => item.swap)
 
     output.saveAsTextFile("output")
 
